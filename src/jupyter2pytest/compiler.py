934907fd24507127dc7c6ae4d47dc36080901bbb
Garrett Credi <garrettcredi@gmail.com>
2023-05-31 14:02:02 -0500

    Use pickle and templatize

--- src/jupyter2pytest/compiler.py
+++ src/jupyter2pytest/compiler.py
@@ -2,24 +2,35 @@ from random import choices
 from string import ascii_lowercase, ascii_uppercase
 from textwrap import indent
 from regex import compile as re_compile
+import pkgutil
 
 from .extractor import TestcellBlock 
 
 clean_pat = re_compile('\W|^(?=\d)')
+
+import os
+
+# with open("./test_file_template.py.template", "r") as f:
+#     test_file_template = f.read()
+#
+# with open("./test_function_template.py.template", "r") as f:
+#     test_function_template = f.read()
+#
+# with open("./test_code_block_template.py.template", "r") as f:
+#     test_code_block_template = f.read()
+
+test_file_template = pkgutil.get_data(__name__, "test_file_template.py.template").decode("utf-8")
+
+test_function_template = pkgutil.get_data(__name__, "test_function_template.py.template").decode("utf-8")
+
+test_code_block_template = pkgutil.get_data(__name__, "test_code_block_template.py.template").decode("utf-8")
 
 def format_testcase_function(
         testcase: str,
         testcase_num: int
     ) -> str:
     testcase_clean = clean_pat.sub("_", testcase)
-    return \
-    f"def test_{testcase_clean}():\n" \
-    "\tpart_completed, error = load_stored_test_data()\n" \
-    f"\tif part_completed < {testcase_num}:\n" \
-    "\t\traise error\n" \
-    "\telse:\n" \
-    "\t\treturn\n"
-    
+    return test_function_template.format(test_name=testcase_clean, test_number=testcase_num)
 def compile_code_and_tests_into_py(
     code_blocks: TestcellBlock,
     ):
@@ -38,49 +49,29 @@ def compile_code_and_tests_into_py(
 
     func_name = choices(ascii_lowercase + ascii_uppercase, k=20)
     func_name = "".join(func_name)
-    py_code = f"import pytest\nimport json\n\npart_completed = -1\ndef {func_name}({part_name}):\n"
-    content = "global part_completed\n"
-
+    
+    test_code = ""
     for testcase in code_blocks.cases:
-        print(f"Adding code for {testcase}")
         code_content = code_blocks.get_code_for_testcase(testcase)
         test_content = code_blocks.get_test_for_testcase(testcase)
+        test_content = indent(test_content, "    ")
+        
+        test_code += test_code_block_template.format(
+            assignment_code = code_content,
+            test_code = test_content,
+            argument = part_name,
+            test_part = testcase
+        )
 
-        content += code_content
-        content += "\n"
-        if len(test_content) > 0:
-            print(f"Adding test for {testcase}")
-            content += f"if {part_name} == \"{testcase}\" or {part_name} == \"all\":\n"
-            test_content += f"\nif {part_name} != \"all\":\n\treturn\n"
-            content += indent(test_content, "\t")
+    test_code = indent(test_code, "    ")
 
-        content += "part_completed = part_completed + 1\n\n"
-
-
-    content += "raise Exception(\"All testcases passed\")\n"
-    py_code += indent(content, "\t")
-
-    py_code += \
-    "def load_stored_test_data():\n" \
-    "\twith open(\"test_data.json\", \"r\") as f:\n" \
-    "\t\tdata = json.load(f)\n" \
-    "\tpart_completed, note = data[\"part_completed\"], data[\"note\"]\n" \
-    "\tif note == \"\":\n" \
-    "\t\treturn part_completed, None\n" \
-    "\terror = Exception(note)\n" \
-    "\treturn part_completed, error\n"
-
+    testcase_functions = ""
     for i, testcase in enumerate(code_blocks.cases):
-        py_code += format_testcase_function(testcase, i)
+        testcase_functions += format_testcase_function(testcase, i)
 
-    py_code += \
-    "if __name__ == \"__main__\":\n" \
-    "\twith pytest.raises(Exception) as e:\n" \
-    f"\t\t{func_name}(\"all\")\n" \
-    "\tnote = \"\"\n" \
-    "\tif \"All testcases passed\" not in str(e.value):\n" \
-    "\t\tnote = str(e.getrepr())\n" \
-    "\tjson_data = { \"part_completed\": part_completed, \"note\": note }\n" \
-    "\twith open(\"test_data.json\", \"w\") as f:\n" \
-    "\t\tf.write(json.dumps(json_data))\n"
-    return py_code
+    return test_file_template.format(
+        function_name = func_name,
+        argument_name = part_name,
+        test_code = test_code,
+        test_functions = testcase_functions
+    )
